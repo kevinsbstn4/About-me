4c1b2a7ccccac3feeca5b7cd12d5439cf1dce518
kevinsbstn4 <kevincaesar22@gmail.com>
2019-12-18 15:43:19 +0700

    about me

---
+++ application/views/about.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <title>Contact</title>
+    <style>
+			/*=-Copyright-=*/
+			.copyright {
+				background-color: #111;
+				padding: 12px 0;
+        font-size:14px;
+			}
+    </style>
+</head>
+<body>
+<div class="jumbotron jumbotron-fluid" style="background-image : url('https://media.istockphoto.com/photos/roadside-with-cityscape-background-picture-id627024026?k=6&m=627024026&s=612x612&w=0&h=CIOz_rpqWPudS6FJgSmtMjfng4DIqpgfen0fOR110Fc='); color: white">
+  <div class="container">
+  <center>
+    <h1 class="display-4"><b><b>About Me</b></b></h1>
+    <p class="lead" style="color: orange; font-family: Arial, Helvetica, sans-serif"><b><b>Posted on 18 Dec</b> 2019</b></p>
+    </center>
+  </div>
+</div>
+<center>
+<div class="card">
+  <div class="card-body" style="font-size: 40px; font-family: Georgia, serif;">
+   About
+  </div>
+</div>
+</center>
+
+<center>
+<table class="table" style="width: 50%; margin: 20px 0px 35px 0px;">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Nama</td>
+      <td>Kevin Caesar Sebastian</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Alamat</td>
+      <td>JL Bima Sakti Utara no.134 </td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Kabupaten</td>
+      <td>Semarang</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Kecamatan</td>
+      <td>Ngaliyan</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Kelurahan</td>
+      <td>Ngaliyan</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>WNA</td>
+      <td>Indonesia</td>
+    </tr>
+  </tbody>
+  </table>
+</table>
+</center>
+<button style="margin: 0px 0px 14px 20px; width: 20%" type="button" class="btn btn-dark" onclick="window.location.href = 'http://localhost/dasarci1/';">Kembali</button>
+		<!-- Copyright -->
+		<section class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 ">
+						<div class="text-center text-white">
+							&copy; 2018 Your Company. All Rights Reserved.
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+</body>
+</html>
---
+++ application/views/home.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- <link rel="stylesheet" type="text/css" href="home.css" /> -->
+    <title>Document</title>
+    <style>
+        .aboutme {
+            display: flex;
+            justify-content: space-between;
+            margin: 40px 12px 60px 12px;
+        }
+        footer {
+				color: #fff;
+				background-attachment: fixed;
+				background-color:#222;
+        background-image: url(https://s7.postimg.org/uyf0oioaz/footer-bg.png);
+				background-size: cover;
+				background-position: bottom;
+			}
+			footer p {
+				color: #ccc;				
+			}
+	   footer a {
+				color: #ccc;				
+			}
+			.social-pet li {
+				display: inline-block;
+				margin-right: 10px;
+			}
+			.social-pet li a {
+				height: 35px;
+				width: 35px;
+				border-radius: 50%;
+				text-align: center;
+				display: block;
+				line-height: 35px;
+				background-color: #3a5a95;
+				color: #fff;
+			}
+			.social-pet li:nth-child(2) a {
+				background-color: #57aced;
+			}
+			.social-pet li:nth-child(3) a {
+				background-color: #dd4f43;
+			}
+			.social-pet li:nth-child(4) a {
+				background-color: #6b27b2;
+			}
+			.social-pet li a:hover {
+				background-color: #0141a2;
+			}
+			.social-pet li a:hover i {
+				transform: rotate(360deg);
+				-moz-transform: rotate(360deg);
+				-webkit-transform: rotate(360deg);
+			}
+			.recent-post li {
+				display: block;
+				color: #ccc;
+				margin-bottom: 25px;
+			}
+			.recent-post li label {
+				float: left;
+				border: 2px solid #ccc;
+				padding: 1px 7px;
+				text-align: center;
+			}
+			.recent-post li label span {
+				color: #fff;
+			}
+			footer .input-group-addon {
+				background-color: #0141a2;
+				padding: 10px;
+			}
+			.f-address li {
+				display: inline-block;
+			}
+			.f-address li i {
+				color: #2995de;
+				font-size: 18px;
+			}
+			.f-address li a {
+				color: #ccc;
+			}
+			/*=-Copyright-=*/
+			.copyright {
+				background-color: #111;
+				padding: 12px 0;
+        font-size:14px;
+			}
+    </style>
+</head>
+<body>
+<div class="jumbotron jumbotron-fluid" style="background-image : url('https://media.istockphoto.com/photos/roadside-with-cityscape-background-picture-id627024026?k=6&m=627024026&s=612x612&w=0&h=CIOz_rpqWPudS6FJgSmtMjfng4DIqpgfen0fOR110Fc='); color: white">
+  <div class="container">
+  <center>
+    <h1 class="display-4"><b><b>About Me</b></b></h1>
+    <p class="lead" style="color: orange; font-family: Arial, Helvetica, sans-serif"><b><b>Posted on 18 Dec</b> 2019</b></p>
+    </center>
+  </div>
+</div>
+<center>
+<div class="card">
+  <div class="card-body" style="font-size: 40px; font-family: Georgia, serif;">
+    This is Website About Me
+  </div>
+</div>
+</center>
+  <!-- ABOUT!!! -->
+<div class="aboutme">
+<div class="card" style="width: 18rem;">
+  <img src="" class="card-img-top" alt="...">
+  <div class="card-body" >
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button class="btn btn-warning" onclick="window.location.href = 'http://localhost/dasarci1/index.php/about/index';">About</button>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button class="btn btn-warning" onclick="window.location.href = 'http://localhost/dasarci1/index.php/about/index';">About</button>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button class="btn btn-warning" onclick="window.location.href = 'http://localhost/dasarci1/index.php/about/index';">About</button>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button class="btn btn-warning" onclick="window.location.href = 'http://localhost/dasarci1/index.php/about/index';">About</button>
+  </div>
+</div>
+  </div>
+
+  <footer class="pt-5 pb-4" id="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
+						<h5 class="mb-4 font-weight-bold">ABOUT US</h5>
+						<p class="mb-4">Etiam laoreet in ex quis efficitur.</p>
+						<ul class="f-address">
+							<li>
+								<div class="row">
+									<div class="col-1"><i class="fas fa-map-marker"></i></div>
+									<div class="col-10">
+										<h6 class="font-weight-bold mb-0">Address:</h6>
+										<p>Ngaliyan,Semarang</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="row">
+									<div class="col-1"><i class="far fa-envelope"></i></div>
+									<div class="col-10">
+										<h6 class="font-weight-bold mb-0">Have any questions?</h6>
+										<p><a href="#">Support@sebastian.com</a></p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="row">
+									<div class="col-1"><i class="fas fa-phone-volume"></i></div>
+									<div class="col-10">
+										<h6 class="font-weight-bold mb-0">Contact:</h6>
+										<p><a href="#">+01 (3) 78 27-5907-4092</a></p>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
+						<h5 class="mb-4 font-weight-bold">FRESH TWEETS</h5>
+						<ul class="f-address">
+							<li>
+								<div class="row">
+									<div class="col-1"><i class="fab fa-twitter"></i></div>
+									<div class="col-10">
+										<p class="mb-0"><a href="#">@userthemesrel </a> HTML Version Out Now</p>
+										<label>10 Mins Ago</label>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="row">
+									<div class="col-1"><i class="fab fa-twitter"></i></div>
+									<div class="col-10">
+										<p class="mb-0"><a href="#">@userthemesrel </a> HTML Version Out Now</p>
+										<label>10 Mins Ago</label>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="row">
+									<div class="col-1"><i class="fab fa-twitter"></i></div>
+									<div class="col-10">
+										<p class="mb-0"><a href="#">@userthemesrel </a> HTML Version Out Now</p>
+										<label>10 Mins Ago</label>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
+						<h5 class="mb-4 font-weight-bold">CONNECT WITH US</h5>
+                        <button type="button" class="btn btn-primary"  onclick="window.location.href = 'http://localhost/dasarci1/index.php/contact/index';">Contact</button>
+				</div>
+			</div>
+		</footer>
+		<!-- Copyright -->
+		<section class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 ">
+						<div class="text-center text-white">
+							&copy; 2018 Your Company. All Rights Reserved.
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+</body>
+</html>
